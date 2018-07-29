2966d6d2442390b954214151114022746085e309
shanbing <shan.bing@foxmail.com>
2018-07-30 00:01:22 +0800

    'update'

--- src/main/java/top/shanbing/domain/model/comment/CommentListRes.java
+++ src/main/java/top/shanbing/domain/model/comment/CommentListRes.java
@@ -2,6 +2,7 @@ package top.shanbing.domain.model.comment;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 
 public class CommentListRes {
     private Integer commentId;
@@ -9,6 +10,64 @@ public class CommentListRes {
     private String commentContacts;
     private String commentContent;
     private String commentDate;
+    private List<ReplyComment> replys;
+
+    public class ReplyComment{
+        private Integer commentId;
+        private String commentName;
+        private String replyCommentName;
+        private String commentContacts;
+        private String commentContent;
+        private String commentDate;
+
+        public Integer getCommentId() {
+            return commentId;
+        }
+
+        public void setCommentId(Integer commentId) {
+            this.commentId = commentId;
+        }
+
+        public String getCommentName() {
+            return commentName;
+        }
+
+        public void setCommentName(String commentName) {
+            this.commentName = commentName;
+        }
+
+        public String getReplyCommentName() {
+            return replyCommentName;
+        }
+
+        public void setReplyCommentName(String replyCommentName) {
+            this.replyCommentName = replyCommentName;
+        }
+
+        public String getCommentContacts() {
+            return commentContacts;
+        }
+
+        public void setCommentContacts(String commentContacts) {
+            this.commentContacts = commentContacts;
+        }
+
+        public String getCommentContent() {
+            return commentContent;
+        }
+
+        public void setCommentContent(String commentContent) {
+            this.commentContent = commentContent;
+        }
+
+        public String getCommentDate() {
+            return commentDate;
+        }
+
+        public void setCommentDate(String commentDate) {
+            this.commentDate = commentDate;
+        }
+    }
 
     public CommentListRes() {
     }
