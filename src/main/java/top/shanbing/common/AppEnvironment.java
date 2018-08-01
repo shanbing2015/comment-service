37de95a6d12e636a17539cd9470ab9918b5c072a
shanbing <shanbing@seeapp.com>
2018-08-01 18:03:48 +0800

    '新增pro环境'

--- src/main/java/top/shanbing/common/AppEnvironment.java
+++ src/main/java/top/shanbing/common/AppEnvironment.java
@@ -1,18 +1,27 @@
 package top.shanbing.common;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+
 @Component
 public class AppEnvironment {
+	protected static Logger logger = LoggerFactory.getLogger(AppEnvironment.class);
 
+	@Value("${deploy.env}")
+	private String env = "pro";
 
-	//@Value("${deploy.env}")
-	private String env = "prod";
+	@PostConstruct
+	public void init(){
+		logger.info("当前环境:"+env);
+	}
 
 	
 	public  boolean isProd(){
-		return "prod".equalsIgnoreCase(env);
+		return "pro".equalsIgnoreCase(env);
 	}
 	
 	public  boolean isDev(){
