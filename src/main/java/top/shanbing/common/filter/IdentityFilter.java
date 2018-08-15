5936d212a6211d20d0c285bd916445fb6bd6d99e
shanbing <shanbing@seeapp.com>
2018-08-15 16:12:15 +0800

    'test1'

---
+++ src/main/java/top/shanbing/common/filter/IdentityFilter.java
@@ -0,0 +1,75 @@
+package top.shanbing.common.filter;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpCookie;
+import org.springframework.http.ResponseCookie;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
+import reactor.core.publisher.Mono;
+import top.shanbing.common.redis.IRedisManager;
+import top.shanbing.util.MD5;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * 身份标识过滤器
+ * @author shanbing
+ * @date 2018/8/15.
+ */
+@Component
+@Order(3)
+public class IdentityFilter implements WebFilter {
+    private static String key = "identity";
+    private static int tt  = 60*60*24*30;//半个月有效期
+
+    private static Map<String,Object> users = new ConcurrentHashMap<>();
+
+    @Autowired
+    private IRedisManager redisManager;
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange serverWebExchange, WebFilterChain webFilterChain) {
+        ServerHttpRequest request =  serverWebExchange.getRequest();
+        ServerHttpResponse response =  serverWebExchange.getResponse();
+
+        MultiValueMap<String, HttpCookie> cookies =  request.getCookies();
+        List<HttpCookie> list = cookies.get(key);
+        Object identity = null;
+        if(list !=null && list.get(0) != null){
+            // todo 验证身份
+            HttpCookie httpCookie = list.get(0);
+            String value = httpCookie.getValue();
+            System.out.println("携带cookie："+value);
+            // todo 本地缓存+redis缓存
+            identity = users.get(value);
+            if(identity == null){
+                identity = redisManager.get(value,Object.class);
+                if(identity != null){
+                    users.put(value,new Object());
+                }
+            }
+        }
+        if(identity == null){
+            // 创建身份
+            String value = MD5.md5(Long.toString(new Date().getTime()));
+            ResponseCookie cookie = ResponseCookie.from(key,value).build();
+            response.addCookie(cookie);
+            // 身份redis缓存
+            users.put(value,new Object());
+            redisManager.setex(value,tt,new Object(),false);
+            System.out.println("创建身份："+ value);
+        }
+        return webFilterChain.filter(serverWebExchange);
+    }
+}
---
+++ src/main/java/top/shanbing/util/MD5.java
@@ -0,0 +1,36 @@
+package top.shanbing.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.math.BigInteger;
+import java.security.MessageDigest;
+
+/**
+ * @author shanbing
+ * @date 2018/8/15.
+ */
+public class MD5 {
+    private static Logger logger = LoggerFactory.getLogger(MD5.class);
+
+    public static String md5(String str){
+        try {
+            // 生成一个MD5加密计算摘要
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            // 计算md5函数
+            md.update(str.getBytes());
+            // digest()最后确定返回md5 hash值，返回值为8为字符串。因为md5 hash值是16位的hex值，实际上就是8位的字符
+            // BigInteger函数则将8位的字符串转换成16位hex值，用字符串来表示；得到字符串形式的hash值
+            return new BigInteger(1, md.digest()).toString(16); //32位小写
+        }catch (Exception e){
+            e.printStackTrace();
+            logger.error("md5错误",e);
+        }
+        return null;
+    }
+
+    public static void main(String[] args) throws Exception  {
+        System.out.println(MD5.md5("shanbing"));
+    }
+
+}
