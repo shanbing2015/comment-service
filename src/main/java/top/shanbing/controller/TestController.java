ceb327884b1c3cb9596a1ee9e9eb985f0725840b
shanbing <shanbing@seeapp.com>
2019-01-11 20:13:53 +0800

    .

--- src/main/java/top/shanbing/controller/TestController.java
+++ src/main/java/top/shanbing/controller/TestController.java
@@ -1,5 +1,7 @@
 package top.shanbing.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +15,7 @@ import java.time.LocalDateTime;
 
 @RestController
 public class TestController {
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
 
     @RequestMapping("/home")
     @FlowRate
@@ -39,9 +42,9 @@ public class TestController {
     }
 
     @RequestMapping("/bypass/notify")
-    public String notifyTest(@RequestBody MailReq req){
-        System.out.println("12306notify"+req.toString());
-        MailUtil.sendCommentNotify(req.toMail,"购票通知","内容:"+req.text);
+    public String notifyTest(String toMail,String msg){
+        log.info("toMail:{},msg:{}",toMail,msg);
+        MailUtil.sendCommentNotify(toMail,"购票订单成功通知","内容:"+msg+"\n\n(请勿非法使用)");
         return "ok\t"+LocalDateTime.now();
     }
 
